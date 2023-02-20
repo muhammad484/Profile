64551bd53bff4275c49cc53502f207d7237cd12a
Muhammad Akbar Maulana <53417278+muhammad484@users.noreply.github.com>
2023-02-20 10:09:13 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -13,27 +13,6 @@
         }
     }
 
-    /**
-     * Easy event listener function
-     */
-    const on = (type, el, listener, all = false) => {
-        let selectEl = select(el, all)
-        if (selectEl) {
-            if (all) {
-                selectEl.forEach(e => e.addEventListener(type, listener))
-            } else {
-                selectEl.addEventListener(type, listener)
-            }
-        }
-    }
-
-    /**
-     * Easy on scroll event listener 
-     */
-    const onscroll = (el, listener) => {
-        el.addEventListener('scroll', listener)
-    }
-
     // Hero Effect 
     const typed = select('.typed')
     if (typed) {
